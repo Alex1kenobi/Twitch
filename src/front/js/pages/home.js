daded0e0cb69535ae2a9017273d6bfad792716d3
andres-av <andres.av.dev@gmail.com>
2022-12-17 19:06:57 +0000

    created sections of home, missing to fix proximas entrevistas

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -27,13 +27,15 @@ export const Home = () => {
               Vota por la pregunta que quieras escuchar o haz tus propias
               preguntas
             </p>
-            <button className="callToAction" role="button">
-              Entrevista a tú manera
-            </button>
+            <a href="#createProfilebtn">
+              <button className="callToAction" role="button">
+                Entrevista a tú manera
+              </button>
+            </a>
           </div>
         </section>
 
-        <section className="pt-5">
+        <section className="pt-5" id="createProfilebtn">
           <div className="container text-center">
             <h2 className="mb-5">Así puedes hacer tus preguntas</h2>
             <div className="homeSection">
@@ -64,13 +66,152 @@ export const Home = () => {
                   <i className="fa-solid fa-circle-question homeFirstSectionIcon"></i>
                 </span>
                 <h3>
-                  Haz tus <span className="preguntasTitle">propias</span> preguntas
+                  Haz tus <span className="preguntasTitle">propias</span>{" "}
+                  preguntas
                 </h3>
                 <p>
                   Si nadie ha hecho la pregunta que estás pensando, no pierdas
                   tiempo, hazla tú mismo y ponla a votación. No te quedes con la
                   duda!
                 </p>
+              </div>
+              <button className="callToAction createProfile" role="button">
+                Crea tu perfil
+              </button>
+            </div>
+          </div>
+        </section>
+
+        <section className="py-5">
+          <div className="container text-center">
+            <div className="homeSection flex-column">
+              <h2 className="my-5">Próximas Entrevistas</h2>
+
+              <div className="col-md-5 mx-auto mb-3">
+                <div
+                  id="carouselExampleIndicators"
+                  class="carousel slide"
+                  data-bs-ride="true"
+                >
+                  <div class="carousel-indicators">
+                    <button
+                      type="button"
+                      data-bs-target="#carouselExampleIndicators"
+                      data-bs-slide-to="0"
+                      class="active"
+                      aria-current="true"
+                      aria-label="Slide 1"
+                    ></button>
+                    <button
+                      type="button"
+                      data-bs-target="#carouselExampleIndicators"
+                      data-bs-slide-to="1"
+                      aria-label="Slide 2"
+                    ></button>
+                    <button
+                      type="button"
+                      data-bs-target="#carouselExampleIndicators"
+                      data-bs-slide-to="2"
+                      aria-label="Slide 3"
+                    ></button>
+                  </div>
+                  <div class="carousel-inner">
+                    <div class="carousel-item active">
+                      <Card />
+                    </div>
+                    <div class="carousel-item">
+                      <Card />
+                    </div>
+                    <div class="carousel-item">
+                      <Card />
+                    </div>
+                  </div>
+                  <button
+                    class="carousel-control-prev"
+                    type="button"
+                    data-bs-target="#carouselExampleIndicators"
+                    data-bs-slide="prev"
+                  >
+                    <span
+                      class="carousel-control-prev-icon"
+                      aria-hidden="true"
+                    ></span>
+                    <span class="visually-hidden">Previous</span>
+                  </button>
+                  <button
+                    class="carousel-control-next"
+                    type="button"
+                    data-bs-target="#carouselExampleIndicators"
+                    data-bs-slide="next"
+                  >
+                    <span
+                      class="carousel-control-next-icon"
+                      aria-hidden="true"
+                    ></span>
+                    <span class="visually-hidden">Next</span>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section className="py-5">
+          <div className="container text-center">
+            <div className="homeSection">
+              <div className="col-md-5 mx-auto d-flex flex-column justify-content-center">
+                <h1 className="mb-4 mt-5">
+                  Quieres saber de qué tratan nuestras entrevistas?
+                </h1>
+                <h4 className="preguntasTitle">
+                  Subtitulo de tematica de entrevistas
+                </h4>
+                <p className="lead">
+                  Fusce at ultricies velit. Morbi cursus turpis nec sodales
+                  mollis. In in accumsan turpis. Morbi feugiat id tellus quis
+                  rhoncus. Morbi porta, ante at fermentum ullamcorper, lectus
+                  nisl dapibus nibh, a rutrum ipsum magna vel orci. Vestibulum
+                  condimentum facilisis lorem, ac ornare mi lacinia quis. Fusce
+                  sodales turpis sed leo gravida, ac viverra nulla sodales.
+                  Fusce in varius dui. Donec iaculis, ipsum placerat scelerisque
+                  volutpat, lacus elit luctus dolor, sed tempus orci nunc et
+                  nunc. Mauris facilisis lobortis lacus hendrerit porta. 
+                  <br></br>
+                  <br></br>
+                  Pellentesque habitant morbi
+                  tristique senectus et netus et malesuada fames ac turpis
+                  egestas. Vestibulum suscipit, massa at congue ornare, arcu
+                  orci venenatis augue, vitae auctor tortor ipsum a tortor.
+                  Nullam egestas risus non erat finibus, non gravida eros
+                  scelerisque. Ut eros ante, vestibulum vitae suscipit sodales,
+                  elementum vitae purus. Curabitur id tincidunt lacus. Maecenas
+                  non magna lorem. Ut accumsan id odio vel iaculis. Donec non
+                  euismod sem, vel semper ipsum.
+                </p>
+              </div>
+              <div className="col-md-5 mx-auto">
+                <img src={superman} className="imgSobremi"></img>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section className="py-5">
+          <div className="container text-center">
+            <div className="homeSection justify-content-around">
+              <div className="col-md-5 d-flex flex-column justify-content-center">
+                <h1 className="mb-4 mt-5">Quieres que te entrevistemos?</h1>
+                <h4>
+                  Si quieres <span className="preguntasTitle">compartir</span>{" "}
+                  algo con la comunidad, contáctanos y podemos agendar una
+                  entrevista
+                </h4>
+                <button className="btn btn-primary mt-5 mb-5" role="button">
+                  Contáctanos
+                </button>
+              </div>
+              <div className="col-md-5">
+                <img src={contactUs} className="contactUs"></img>
               </div>
             </div>
           </div>
@@ -84,59 +225,27 @@ export const Home = () => {
               </div>
               <div className="col-md-5 mx-auto">
                 <h1 className="mb-4 mt-5">Sobre Mí</h1>
-                <h4 className="preguntasTitle">Consultor Empresarial y Digital</h4>
+                <h4 className="preguntasTitle">
+                  Consultor Empresarial y Digital
+                </h4>
                 <p className="lead">
                   Escucha la historia de mi vida de cómo el destino cambió mi
                   movida, de cómo sin comerlo ni beberlo llegué a ser el
-                  pringado de un barrio llamado Bel Air. 
+                  pringado de un barrio llamado Bel Air.
                   <br></br>
                   <br></br>
-                  Al oeste de un islote
-                  crecía y vivía sin hacer mucho caso a la policía. Jugaba al
-                  Call of Duty sin cansarme demasiado porque por las noches me
-                  sacaba el graduado. Cierto día jugando Fifa con unos amigos
-                  unos tipos me metieron en un lío y mi madre me decía una y
-                  otra vez: “¡Con tu tío y tu tía aprenderás Photoshop!”
+                  Al oeste de un islote crecía y vivía sin hacer mucho caso a la
+                  policía. Jugaba al Call of Duty sin cansarme demasiado porque
+                  por las noches me sacaba el graduado. Cierto día jugando Fifa
+                  con unos amigos unos tipos me metieron en un lío y mi madre me
+                  decía una y otra vez: “¡Con tu tío y tu tía aprenderás
+                  Photoshop!”
                 </p>
-                <a className="btn btn-primary mb-5 mt-4" href="#">
-                  Quieres saber más de mí?
-                </a>
               </div>
             </div>
           </div>
         </section>
-        <section className="py-5">
-          <div className="container text-center">
-            <div className="homeSection">
-              <div className="col-md-5 mx-auto d-flex flex-column justify-content-center">
-                <h1 className="mb-4 mt-5">Hazte un perfil</h1>
-                <h4>Y empieza a disfrutar de <span className="preguntasTitle">decenas</span> de entrevistas cada mes</h4>
-                <button className="btn btn-primary mt-5" role="button">
-                  Entrevista a tú manera
-                </button>
-              </div>
-              <div className="col-md-5 mx-auto">
-                <img src={superman} className="imgSobremi"></img>
-              </div>
-            </div>
-          </div>
-          </section>
-        <section className="py-5">
-          <div className="container text-center">
-            <div className="homeSection">
-              <div className="col-md-5 mx-auto">
-                <img src={contactUs} className="contactUs"></img>
-              </div>
-              <div className="col-md-5 mx-auto d-flex flex-column justify-content-center">
-                <h1 className="mb-4 mt-5">Quieres que te entrevistemos?</h1>
-                <h4>Si quieres <span className="preguntasTitle">compartir</span> algo con la comunidad, contáctanos y podemos agendar una entrevista</h4>
-                <button className="btn btn-primary mt-5 mb-5" role="button">
-                  Contáctanos
-                </button>
-              </div>
-            </div>
-          </div>
-          </section>
+
       </div>
     );
 }
